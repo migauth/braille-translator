e8af81c941375ec9c7f804470bd79daed90aaf69
Michael Gauthier <mikegauthier222@hotmail.com>
2024-06-20 10:44:39 -0700

    add input component and basic tailwind structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
+<script setup>
+import TranslatorInput from './components/TranslatorInput.vue'
+</script>
+
 <template>
-  <main>
-    <div>Home</div>
+  <main class="">
+    <h1 class="text-9xl">Welcome to Simple Braille Translator</h1>
+    <!-- Translator -->
+    <div>
+      <TranslatorInput></TranslatorInput>
+    </div>
   </main>
 </template>
---
+++ src/components/TranslatorInput.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div>
+    <form>
+      <div class="">
+        <input
+          type="text"
+          placeholder="Type here"
+          class="w-fit border-8 border-dashed border-white bg-black rounded-md placeholder-yellow-300 text-4xl p-6"
+        />
+      </div>
+    </form>
+  </div>
+</template>
