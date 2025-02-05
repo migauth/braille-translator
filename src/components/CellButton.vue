0ac40b9446c8d70fa686dc823988ed637008f2a3
Michael Gauthier <mikegauthier222@hotmail.com>
2025-02-05 09:22:04 -0800

    add button styles

--- src/components/CellButton.vue
+++ src/components/CellButton.vue
@@ -1,7 +1,15 @@
-<script>
+<script setup>
+import { computed } from 'vue';
+import { siteStyles } from '@/state'
+
+const buttonClass = computed(() => {
+  return `${siteStyles.value.border} ${siteStyles.value.text} ${siteStyles.value.text} ${siteStyles.value.padding}`
+})
 
 </script>
 
 <template>
-  <button class=" font-semibold rounded-md p-2 text-3xl sm:text-2xl mb-3 w-full sm:w-auto">⠀</button>
+  <button :class="buttonClass"
+    class=" font-semibold rounded-md p-2 text-3xl sm:text-6xl mb-3 w-full sm:w-auto">⠀</button>
+
 </template>
